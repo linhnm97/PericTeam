781f6082b34a8bdf59957dc4d253b4f32bdae72e
linhnm97 <manhlinhnguyen51@gmail.com>
2022-08-05 23:47:23 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import { Route, Routes} from 'react-router-dom';
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Products from './components/Products';
+import Product from './components/Product';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+          <Route path="/" element={ <Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path='/products/:id' element={<Product />}/>
+          <Route path='/cart' element={<Cart />}/>
+          <Route path='/checkout' element={<Checkout />}/>
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
